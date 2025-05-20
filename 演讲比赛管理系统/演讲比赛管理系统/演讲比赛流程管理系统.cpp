9bde31c4d5e49690c54a363d430e7eb271782331
suncheng0 <161986369+suncheng0@users.noreply.github.com>
2025-05-20 19:08:39 +0800

    Add files via upload

---
+++ 演讲比赛管理系统/演讲比赛管理系统/演讲比赛流程管理系统.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<ctime>
+using namespace std;
+#include"speechManager.h"
+
+
+int main()
+{
+	srand((unsigned int)time(NULL));
+	SpeechManager sm;
+	/*sm.show_Menu();*/
+	//for (map<int, Speaker>::iterator it = sm.m_Speaker.begin(); it != sm.m_Speaker.end(); it++)
+	//{
+	//	cout << "选手编号" << it->first << "姓名" << it->second.m_Name << "分数" << it->second.m_Score[0] << endl;
+	//}
+	
+	int choice = 0;
+	while (true)
+	{
+		sm.show_Menu();
+		cout << "请输入您的选择" << endl;
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			sm.startSpeech();
+			break;
+		case 2:
+			sm.loadRecord();
+			break;
+		case 3:
+			sm.clearRecord();
+			break;
+		case 0:
+			sm.exitSystem();
+			break;
+		default:
+			system("cls");
+			break;
+		}
+	}
+	system("pause");
+	return 0;
+}
